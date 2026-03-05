41537ab2464604e6e881f91fc9195d3ba54025c2
TriLau <157219246+TriLau@users.noreply.github.com>
2026-03-05 20:33:21 +0700

    Update 'MazeScene'
    Update random map generate function

---
+++ Assets/TriLauAsset/Script/MapGenerateSystem/NodeBlueprint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace MyRule
+{
+    public enum NodeType
+    {
+        MinorEnemy,
+        EliteEnemy,
+        RestSite,
+        Treasure,
+        Store,
+        Boss,
+        Mystery
+    }
+}
+
+namespace MyRule
+{
+    [CreateAssetMenu]
+    public class NodeBlueprint : ScriptableObject
+    {
+        public Sprite sprite;
+        public NodeType nodeType;
+    }
+}
